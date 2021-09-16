7280ab4162a56e81e377d3f29a255e0f41a1c112
Okey Obiajunwa <okeyobia@Okeys-MacBook-Air.local>
2021-09-16 05:56:48 -0500

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+const Header = () => {
+  return (
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+      <Image
+        className='object-contain'
+        src='/images/hulu-logo-white.png'
+        width={100}
+        height={50}
+      />
+    </header>
+  );
+};
+
+export default Header;
